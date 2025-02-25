7312368f377273b35f9aec9683e3d3b6c38277c5
Fatimah Zahra Adam <92226557+zahrawm@users.noreply.github.com>
2025-02-25 09:08:50 +0000

    Codes updated

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/screen/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: LoginScreen(),
     );
   }
 }
